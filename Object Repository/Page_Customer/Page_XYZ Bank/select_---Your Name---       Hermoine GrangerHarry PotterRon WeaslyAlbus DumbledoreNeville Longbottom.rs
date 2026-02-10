bb9d1d308a15dfa2f6d9dc19f2a47ab25c8d7797
Putra Alrasy <dianaputra529@gmail.com>
2026-02-10 21:42:22 +0800

    Commit repo

---
+++ Object Repository/Page_Customer/Page_XYZ Bank/select_---Your Name---       Hermoine GrangerHarry PotterRon WeaslyAlbus DumbledoreNeville Longbottom.rs
@@ -0,0 +1,133 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>select_---Your Name---       Hermoine GrangerHarry PotterRon WeaslyAlbus DumbledoreNeville Longbottom</name>
+   <tag></tag>
+   <elementGuidId>61d24d1a-4787-4605-9957-320ce0802fa5</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//select[@id='userSelect']</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>#userSelect</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>select</value>
+      <webElementGuid>3b89cc28-4ac2-48b8-b9da-bc2c2dd65d15</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>form-control ng-pristine ng-untouched ng-valid</value>
+      <webElementGuid>03f26810-86b3-4430-89f2-f32cf798c2a3</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>name</name>
+      <type>Main</type>
+      <value>userSelect</value>
+      <webElementGuid>f38099a6-8e5a-4f78-9cca-6c3c9d46eb2f</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>id</name>
+      <type>Main</type>
+      <value>userSelect</value>
+      <webElementGuid>3261bfd2-cc86-4e98-81a2-c80b7473a843</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>ng-model</name>
+      <type>Main</type>
+      <value>custId</value>
+      <webElementGuid>6ca779a7-ad0c-4368-be81-42bf0d90b305</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+	      ---Your Name--- 
+	      Hermoine GrangerHarry PotterRon WeaslyAlbus DumbledoreNeville Longbottom
+	    </value>
+      <webElementGuid>2ac72c58-0cd1-4792-bbad-35dce2e6450f</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;userSelect&quot;)</value>
+      <webElementGuid>a8ec7406-78dc-4757-b2ce-5d24e2bfcbe8</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:attributes</name>
+      <type>Main</type>
+      <value>//select[@id='userSelect']</value>
+      <webElementGuid>a1606ca4-4b41-425b-8c4f-b0ac2500200a</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Your Name :'])[1]/following::select[1]</value>
+      <webElementGuid>9090d71d-718c-403f-81c8-5ff56599a045</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Logout'])[1]/following::select[1]</value>
+      <webElementGuid>59a422a7-3309-41be-9b65-37e415b45a81</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Login'])[1]/preceding::select[1]</value>
+      <webElementGuid>12ff8793-fbe6-45ae-b704-5d27fbd0f76a</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//select</value>
+      <webElementGuid>cb7a4ff0-4c6b-4c18-b3ae-ad89a851b348</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//select[@name = 'userSelect' and @id = 'userSelect' and (text() = '
+	      ---Your Name--- 
+	      Hermoine GrangerHarry PotterRon WeaslyAlbus DumbledoreNeville Longbottom
+	    ' or . = '
+	      ---Your Name--- 
+	      Hermoine GrangerHarry PotterRon WeaslyAlbus DumbledoreNeville Longbottom
+	    ')]</value>
+      <webElementGuid>6eeefe01-67c1-4163-94f6-33925d66cf2b</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
