36f111f8ecfb622f715ec7d2a304282a485f550f
Putra Alrasy <dianaputra529@gmail.com>
2026-02-11 09:36:35 +0800

    Update data, add Test Suite

---
+++ Test Suites/Customer/TS_Customer_Login_DataDriven.ts
@@ -0,0 +1,36 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_Customer_Login_DataDriven</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>10</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>5b06e375-d92c-435f-8f03-13914f217ab6</testSuiteGuid>
+   <testCaseLink>
+      <guid>0dbc953f-fa56-43cb-a02d-b756a11a2124</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Customer_Page</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>57d5cd5c-ed98-4f6e-89d8-42906ce11f7f</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/List_Customer_Name</testDataId>
+      </testDataLink>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId>57d5cd5c-ed98-4f6e-89d8-42906ce11f7f</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>CustomerName</value>
+         <variableId>d88b9c42-a8c5-45d6-b907-21dabe5ad410</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
