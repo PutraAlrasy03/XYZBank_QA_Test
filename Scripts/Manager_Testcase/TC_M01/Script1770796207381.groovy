f7279e21f6621f8a4f669896327a7c07708954f9
Putra Alrasy <dianaputra529@gmail.com>
2026-02-11 16:14:53 +0800

    TC:Updated comment

--- Scripts/Manager_Testcase/TC_M01/Script1770796207381.groovy
+++ Scripts/Manager_Testcase/TC_M01/Script1770796207381.groovy
@@ -5,5 +5,4 @@ import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 WebUI.click(findTestObject('Manager_Obj/Page_XYZ Bank/button_Bank Manager Login'))
 
 // 2. Verify that the user is successfully routed to the Bank Manager dashboard
-// Notice there is NO comma or number 5 at the end of this line now!
 WebUI.verifyElementVisible(findTestObject('Manager_Obj/Page_XYZ Bank/button_Open Account'))
